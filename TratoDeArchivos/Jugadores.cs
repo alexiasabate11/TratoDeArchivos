f661e922fd615c0fb3c2b732e150bae54639ce26
Abel Cumbreño <abelcumbrenobalmes@gmail.com>
2024-09-12 13:54:44 +0200

    V.2 - Eliminar la clase jugadores y volverla a crear correctamente :)

---
+++ TratoDeArchivos/Jugadores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratoDeArchivos
+{
+    internal class Jugadores
+    {
+        int dorsal;
+        string nombrejugador;
+        string nombreEquipo;
+
+        public int Dorsal { get => dorsal; set => dorsal = value; }
+        public string Nombrejugador { get => nombrejugador; set => nombrejugador = value; }
+        public string NombreEquipo { get => nombreEquipo; set => nombreEquipo = value; }
+    }
+}
