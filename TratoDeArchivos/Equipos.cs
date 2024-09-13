b278f5fe2f3de17d51ec4500f97c16d0df090631
alexiasabate11 <alexiablackha@gmail.com>
2024-09-13 21:22:36 +0200

    Creada la clase equpos correcta

---
+++ TratoDeArchivos/Equipos.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TratoDeArchivos
+{
+    internal class Equipos
+    {
+        static string nombreEquipo;
+        static int puntuacionEquipo;
+
+        public Equipos(string NombreEquipo, int PuntuacionEquipo)
+        {
+            nombreEquipo = NombreEquipo;
+            puntuacionEquipo = PuntuacionEquipo;
+        }
+
+        public string NombreEquipo
+        {
+            get => nombreEquipo;
+            set => nombreEquipo = value;
+        }
+
+        public int PuntuacionEquipo
+        {
+            get => puntuacionEquipo;
+            set => puntuacionEquipo = value;
+        }
+
+        public void MostrarInformacion()
+        {
+            Console.WriteLine($"Equipo: {nombreEquipo}, Puntuacion: {puntuacionEquipo}");
+        }
+
+    }
+}
